03ecf353be6eb5db5516fb525edd5dc0683ba356
YetiGonk <maxjlopez02@gmail.com>
2025-04-29 23:34:55 +0100

    updated hires and card_library

--- card_library.py
+++ card_library.py
@@ -320,41 +320,39 @@ CARD_LIBRARY = {
         "description": "Can be used as a diamond or heart card of value 10"
     },
     
-    # Diamond Jack Hires
-    "diamonds_11_archer": {
-        "suit": "diamonds",
+    # Jack Hires
+    "hires_11_archer": {
+        "suit": "hires",
         "value": 11,
         "unlocked": False,
         "owned": 0,
         "rarity": RARE,
         "hireable": True,
         "hireable_type": ARCHER,
-        "description": "When activated, strikes a monster in the room dealing 3 damage. If this defeats the monster, you take no damage from it."
+        "description": "When activated, strikes a monster in the room reducing its value by 3."
     },
-    "diamonds_11_berserker": {
-        "suit": "diamonds",
+    "hires_11_berserker": {
+        "suit": "hires",
         "value": 11,
         "unlocked": False,
         "owned": 0,
         "rarity": RARE,
         "hireable": True,
         "hireable_type": BERSERKER,
-        "description": "When activated, doubles your current weapon's value for your next monster battle, but you must attack a monster immediately after using this hire."
+        "description": "When activated, doubles your current weapon's value up to ace (14) for your next monster battle, but you must attack a monster immediately after using this hire. If the next card you interact with isn't a monster card, the weapon reverts back to its original value. If it is a monster card, the card will remain doubled until discarded."
     },
-    "diamonds_11_prisoner": {
-        "suit": "diamonds",
+    "hires_11_prisoner": {
+        "suit": "hires",
         "value": 11,
         "unlocked": False,
         "owned": 0,
         "rarity": RARE,
         "hireable": True,
         "hireable_type": PRISONER,
-        "description": "When activated, sacrifices 2 health to remove one monster card from the current room and place it at the bottom of the dungeon deck."
+        "description": "When activated, sacrifices 2 health to discard a selected monster card from the current room and place it at the bottom of the floor deck."
     },
-    
-    # Heart Jack Hires
-    "hearts_11_bard": {
-        "suit": "hearts",
+    "hires_11_bard": {
+        "suit": "hires",
         "value": 11,
         "unlocked": False,
         "owned": 0,
@@ -363,18 +361,18 @@ CARD_LIBRARY = {
         "hireable_type": BARD,
         "description": "When activated, reveals one random room in the dungeon (4 cards) and allows you to swap one card from your current room with any card from the revealed room."
     },
-    "hearts_11_miner": {
-        "suit": "hearts",
+    "hires_11_miner": {
+        "suit": "hires",
         "value": 11,
         "unlocked": False,
         "owned": 0,
         "rarity": RARE,
         "hireable": True,
         "hireable_type": MINER,
-        "description": "When activated, searches the dungeon deck and retrieves one diamond card with value 5 or lower to add to your inventory."
+        "description": "When activated, searches the dungeon deck and retrieves one diamond weapon card with value 5 or lower to add to your inventory."
     },
-    "hearts_11_thief": {
-        "suit": "hearts",
+    "hires_11_thief": {
+        "suit": "hires",
         "value": 11,
         "unlocked": False,
         "owned": 0,
@@ -394,7 +392,7 @@ CARD_LIBRARY = {
         "rarity": EPIC,
         "hireable": False,
         "hireable_type": None,
-        "description": "Can be used as any Jack hireable card"
+        "description": "Can be used as a diamond weapon or heart potion card of value 11."
     },
     
     # Wildcard Queen
@@ -406,64 +404,62 @@ CARD_LIBRARY = {
         "rarity": EPIC,
         "hireable": False,
         "hireable_type": None,
-        "description": "Can be used as any Queen hireable card"
+        "description": "Can be used as a diamond weapon or heart potion card of value 12."
     },
     
-    # Diamond Queen Hires
-    "diamonds_12_blacksmith": {
-        "suit": "diamonds",
+    # Queen Hires
+    "hires_12_blacksmith": {
+        "suit": "hires",
         "value": 12,
         "unlocked": False,
         "owned": 0,
         "rarity": EPIC,
         "hireable": True,
         "hireable_type": BLACKSMITH,
-        "description": "When activated, enhances your currently equipped weapon by +3 value. If you have no weapon equipped, crafts a diamond 5 weapon for you."
+        "description": "When activated, enhances your currently equipped weapon by +5 value up to ace (14). If you have no weapon equipped, crafts and equips a diamond 7 weapon for you."
     },
-    "diamonds_12_magician": {
-        "suit": "diamonds",
+    "hires_12_magician": {
+        "suit": "hires",
         "value": 12,
         "unlocked": False,
         "owned": 0,
         "rarity": EPIC,
         "hireable": True,
         "hireable_type": MAGICIAN,
-        "description": "When activated, lets you choose one card from the room to duplicate. The duplicate is added to your inventory if it's a diamond or heart, or removed from play if it's a monster."
+        "description": "When activated, the magician lets you choose one card from the room to duplicate. The duplicate is added to your inventory if it's a diamond or heart. If it's a monster card, it is added to the room. If the room is already at full capacity (4) or your inventory is full, then the duplicate is discarded."
     },
-    "diamonds_12_tamer": {
-        "suit": "diamonds",
+    "hires_12_tamer": {
+        "suit": "hires",
         "value": 12,
         "unlocked": False,
         "owned": 0,
         "rarity": EPIC,
         "hireable": True,
         "hireable_type": TAMER,
-        "description": "When activated, tames one monster in the room, adding it to your inventory as a special weapon with value equal to the monster's value divided by 2 (rounded up)."
+        "description": "When activated, tames one monster in the room, adding it to your inventory as a special weapon with value equal to the monster's value -2."
     },
-    
-    # Heart Queen Hires
-    "hearts_12_clown": {
-        "suit": "hearts",
+    "hires_12_clown": {
+        "suit": "hires",
         "value": 12,
         "unlocked": False,
         "owned": 0,
         "rarity": EPIC,
         "hireable": True,
         "hireable_type": CLOWN,
-        "description": "When activated, shuffles all cards in the current room back into the dungeon deck and draws 4 new cards to replace them. You cannot run from the new room."
+        "description": "When activated, shuffles all cards in the current room back into the dungeon deck and draws 4 new cards to replace them. You cannot run from the new room. You can run from the next room after this one."
     },
-    "hearts_12_druid": {
-        "suit": "hearts",
+    "hires_12_druid": {
+        "suit": "hires",
         "value": 12,
         "unlocked": False,
         "owned": 0,
         "rarity": EPIC,
         "hireable": True,
         "hireable_type": DRUID,
-        "description": "When activated, transforms all heart cards in the room, increasing their value by 2 (maximum 10). If there are no heart cards, transforms the lowest value monster card into a heart card of equal value."
+        "description": "When activated, transforms all heart cards in the room, increasing their value by 2 up to ace (14). If there are no heart cards, transforms the lowest value monster card in the room into a heart card of equal value."
     },
-    "hearts_12_nun": {
-        "suit": "hearts",
+    "hires_12_nun": {
+        "suit": "hires",
         "value": 12,
         "unlocked": False,
         "owned": 0,
@@ -498,9 +494,9 @@ CARD_LIBRARY = {
         "description": "Can be used as any diamond or heart card of any value"
     },
     
-    # Diamond King Hires
-    "diamonds_13_fighter": {
-        "suit": "diamonds",
+    # King Hires
+    "hires_13_fighter": {
+        "suit": "hires",
         "value": 13,
         "unlocked": False,
         "owned": 0,
@@ -509,8 +505,8 @@ CARD_LIBRARY = {
         "hireable_type": FIGHTER,
         "description": "When activated, advances into the dungeon and destroys the next two monsters in the deck, regardless of their value."
     },
-    "diamonds_13_samurai": {
-        "suit": "diamonds",
+    "hires_13_samurai": {
+        "suit": "hires",
         "value": 13,
         "unlocked": False,
         "owned": 0,
@@ -519,20 +515,18 @@ CARD_LIBRARY = {
         "hireable_type": SAMURAI,
         "description": "When activated, instantly defeats all monsters currently in the room without taking any damage, regardless of your weapon or their values."
     },
-    "diamonds_13_shooter": {
-        "suit": "diamonds",
+    "hires_13_shooter": {
+        "suit": "hires",
         "value": 13,
         "unlocked": False,
         "owned": 0,
         "rarity": RELIC,
         "hireable": True,
         "hireable_type": SHOOTER,
-        "description": "When activated, snipes the highest value monster card from each of the next 2 rooms and removes them from the dungeon deck."
+        "description": "When activated, snipes the highest value monster card from each of the next 2 rooms and discards them."
     },
-    
-    # Heart King Hires
-    "hearts_13_merchant": {
-        "suit": "hearts",
+    "hires_13_merchant": {
+        "suit": "hires",
         "value": 13,
         "unlocked": False,
         "owned": 0,
@@ -541,24 +535,24 @@ CARD_LIBRARY = {
         "hireable_type": MERCHANT,
         "description": "When activated, shuffles the discard pile and draws the top four red cards. The player can then purchase one card of the four. Hearts are free but heal only 3 health. Diamonds cost 3 health to purchase."
     },
-    "hearts_13_necromancer": {
-        "suit": "hearts",
+    "hires_13_necromancer": {
+        "suit": "hires",
         "value": 13,
         "unlocked": False,
         "owned": 0,
         "rarity": RELIC,
         "hireable": True,
         "hireable_type": NECROMANCER,
-        "description": "When activated, select a monster (spades or clubs) card in the room and the necromancer uses undead magic to convert it into a potion, turning it into a heart card of equal value."
+        "description": "When activated, necromancer uses undead magic to convert all monsters in the room into diamond cards of equal value."
     },
-    "hearts_13_warrior": {
-        "suit": "hearts",
+    "hires_13_warrior": {
+        "suit": "hires",
         "value": 13,
         "unlocked": False,
         "owned": 0,
         "rarity": RELIC,
         "hireable": True,
         "hireable_type": WARRIOR,
-        "description": "When activated, grants you temporary invulnerability, allowing you to defeat the next 3 monsters you encounter without taking any damage, regardless of their value or your weapon."
+        "description": "When activated, grants you temporary invulnerability, allowing you to defeat the next 4 monsters you encounter without taking any damage, regardless of their value or your weapon."
     }
 }
